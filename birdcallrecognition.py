aec75ff6ff400d0059974eb43b93f7554be13d85
AndrewCalder <andrewglenh@gmail.com>
2018-11-17 19:43:10 -0600

    Initial commit

---
+++ birdcallrecognition.py
@@ -0,0 +1,9 @@
+#C S 4033-001 Machine Learning
+#Short SL Project - Bird Call Recognition
+#David McKnight, Cody Degner, Andrew Calder
+
+#Import packages
+import pyAudioAnalysis;
+
+#Testing
+print("Hello world");
